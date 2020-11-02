7c34feb0ac94bbaaf8fb74de4d873c0247bbe421
fullsnack-DEV <pratikpatre2019@gmail.com>
2020-11-02 16:31:39 +0530

    Added the flatlist Animation

--- Screens/HomesScreen.js
+++ Screens/HomesScreen.js
@@ -14,9 +14,73 @@ const WIDTH = Dimensions.get("window").width;
 
 const HEIGHT = Dimensions.get("window").height;
 
+const LOGO_WIDTH = 220;
+
+const LOGO_HEIGHT = 40;
+
+const TICKER_HEIGHT = 50;
+
+const CIRCLE_SIZE = WIDTH * 0.6;
+
 import data from "../Data/data";
 
 import ListitemCom from "../component/ListitemCom";
+
+const Circle = ({ ScrollX }) => {
+  return (
+    <View style={[styles.circleContainer, StyleSheet.absoluteFillObject]}>
+      {data.map(({ dotBg }, index) => {
+        const inputRange = [
+          (index - 0.55) * WIDTH,
+          index * WIDTH,
+          (index + 0.55) * WIDTH,
+        ];
+        const scale = ScrollX.interpolate({
+          inputRange,
+          outputRange: [0, 1, 0],
+          extrapolate: "clamp",
+        });
+
+        const opacity = ScrollX.interpolate({
+          inputRange,
+          outputRange: [0, 0.2, 0],
+        });
+
+        return (
+          <Animated.View
+            key={index}
+            style={[
+              styles.circle,
+              { opacity, backgroundColor: dotBg, transform: [{ scale }] },
+            ]}
+          />
+        );
+      })}
+    </View>
+  );
+};
+
+const Ticker = ({ ScrollX }) => {
+  const inputRange = [-WIDTH, 0, WIDTH];
+  const translateY = ScrollX.interpolate({
+    inputRange,
+    outputRange: [TICKER_HEIGHT, 0, -TICKER_HEIGHT],
+  });
+
+  return (
+    <View style={styles.tickercontainer}>
+      <Animated.View style={{ transform: [{ translateY }] }}>
+        {data.map(({ type }, index) => {
+          return (
+            <Text key={index} style={styles.tickertxt}>
+              {type}
+            </Text>
+          );
+        })}
+      </Animated.View>
+    </View>
+  );
+};
 
 export default function HomesScreen({}) {
   const ScrollX = useRef(new Animated.Value(0)).current; //Intializing the new animated.Value. We will then map this SCrollX to the flatlist so that we can aimate it.
@@ -25,7 +89,7 @@ export default function HomesScreen({}) {
     <SafeAreaView style={styles.container}>
       <View>
         <StatusBar style="auto" hidden />
-
+        <Circle ScrollX={ScrollX} />
         <Animated.FlatList
           data={data}
           horizontal
@@ -40,7 +104,13 @@ export default function HomesScreen({}) {
           )}
           scrollEventThrottle={16} // To get a 60 FPS .
         />
+        <Image
+          style={styles.logo}
+          source={require("../assets/img/ue_black_logo.png")}
+        />
       </View>
+
+      <Ticker ScrollX={ScrollX} />
     </SafeAreaView>
   );
 }
@@ -48,5 +118,48 @@ export default function HomesScreen({}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#8fB0bc",
+  },
+  logo: {
+    opacity: 0.9,
+    position: "absolute",
+    height: LOGO_HEIGHT,
+    width: LOGO_WIDTH,
+    resizeMode: "contain",
+    left: 10,
+    bottom: 10,
+    transform: [
+      { translateX: -LOGO_WIDTH / 2 },
+      { translateY: -LOGO_HEIGHT / 2 },
+      { rotateZ: "-90deg" },
+      { translateX: LOGO_WIDTH / 2 },
+      { translateY: LOGO_HEIGHT / 2 },
+    ],
+  },
+  tickercontainer: {
+    position: "absolute",
+    top: 50,
+    left: 20,
+    overflow: "hidden",
+    height: TICKER_HEIGHT,
+  },
+  tickertxt: {
+    fontSize: TICKER_HEIGHT / 1.5,
+    lineHeight: TICKER_HEIGHT,
+    textTransform: "uppercase",
+    letterSpacing: 2,
+    fontWeight: "800",
+    fontFamily: "nunito-bold",
+  },
+  circleContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  circle: {
+    width: CIRCLE_SIZE,
+    height: CIRCLE_SIZE,
+    borderRadius: CIRCLE_SIZE / 2,
+    position: "absolute",
+    top: HEIGHT / 8,
   },
 });
--- component/ListitemCom.js
+++ component/ListitemCom.js
@@ -8,6 +8,7 @@ import {
   SafeAreaView,
   Animated,
 } from "react-native";
+import data from "../Data/data";
 
 const WIDTH = Dimensions.get("window").width;
 
@@ -22,8 +23,26 @@ export default function ListitemCom({
 }) {
   //We have to create a Input range so that we can scale the image.
   const inputRange = [(index - 1) * WIDTH, index * WIDTH, (index + 1) * WIDTH];
+  const inputRangeOpacity = [
+    (index - 0.2) * WIDTH,
+    index * WIDTH,
+    (index + 0.2) * WIDTH,
+  ];
   const scale = ScrollX.interpolate({
     inputRange,
+    outputRange: [0, 1, 0],
+  });
+  const translateXheading = ScrollX.interpolate({
+    inputRange,
+    outputRange: [WIDTH * 0.2, 0, -WIDTH * 0.2],
+  });
+
+  const translateXdescription = ScrollX.interpolate({
+    inputRange,
+    outputRange: [WIDTH * 0.8, 0, -WIDTH * 0.8],
+  });
+  const opacity = ScrollX.interpolate({
+    inputRange: inputRangeOpacity,
     outputRange: [0, 1, 0],
   });
 
@@ -32,11 +51,32 @@ export default function ListitemCom({
       <View style={styles.container}>
         <Animated.Image
           source={imageUri}
+          an
           style={[styles.imgstyle, { transform: [{ scale }] }]}
         />
         <View style={styles.textcontainer}>
-          <Text style={styles.heading}>{heading}</Text>
-          <Text style={styles.description}>{description}</Text>
+          <Animated.Text
+            style={[
+              styles.heading,
+              {
+                opacity,
+                transform: [{ translateX: translateXheading }],
+              },
+            ]}
+          >
+            {heading}
+          </Animated.Text>
+          <Animated.Text
+            style={[
+              styles.description,
+              {
+                opacity,
+                transform: [{ translateX: translateXdescription }],
+              },
+            ]}
+          >
+            {description}
+          </Animated.Text>
         </View>
       </View>
     </SafeAreaView>
@@ -64,7 +104,7 @@ const styles = StyleSheet.create({
     flex: 0.5,
   },
   heading: {
-    color: "#444",
+    color: "#fff",
     textTransform: "uppercase",
     fontSize: 24,
     letterSpacing: 2,
@@ -73,7 +113,7 @@ const styles = StyleSheet.create({
     fontFamily: "nunito-bold",
   },
   description: {
-    color: "#ccc",
+    color: "#000",
     fontWeight: "600",
     textAlign: "left",
     width: WIDTH * 0.75,
